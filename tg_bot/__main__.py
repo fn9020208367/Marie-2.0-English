833fbf67d0f7f13a3f9a1c0447e53c06de60f127
PROFESSOR <66514907+Adithyan1133-ctrl@users.noreply.github.com>
2020-08-30 13:09:39 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -45,7 +45,7 @@ Hello! my name *{}*.
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll of the following commands  / or ! can  be used...\n")
 
-DONATE_STRING = [Click here](https://t.me/MalayaliLinksBot) if you or donate.
+DONATE_STRING = """[Click here](https://t.me/MalayaliLinksBot) if you or donate.
 
 
 There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
